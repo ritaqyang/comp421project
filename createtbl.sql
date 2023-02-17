797d9260e5cd4e2725dd9e94f19f086fb293ec4d
ritaqyang <ritaqyang@gmail.com>
2023-02-17 12:56:29 -0500

    added two create table statements

--- createtbl.sql
+++ createtbl.sql
@@ -9,11 +9,70 @@ CONNECT TO cs421;
 
 -- This is only an example of how you add create table ddls to this file.
 --   You may remove it.
-CREATE TABLE MYTEST01
+
+CREATE TABLE Teams
 (
-  id INTEGER NOT NULL
- ,value INTEGER
- ,PRIMARY KEY(id)
+    country     CHAR(20),
+    association CHAR(40) NOT NULL,
+    URL         CHAR(40) NOT NULL,
+    group       INTEGER  NOT NULL,
+    PRIMARY KEY (country)
 );
 
+CREATE TABLE Players
+(
+    country   CHAR(20),
+    shirt_num INTEGER,
+    pname     CHAR(40) NOT NULL,
+    position  CHAR(20) NOT NULL,
+    DOB       DATE     NOT NULL,
+    PRIMARY KEY (country, shirt_num),
+    FOREIGN KEY (country) REFERENCES Teams
+);
+
+Coaches (country, cname, DOB, role)
+(
+    country   CHAR(20),
+    pname     CHAR(40),
+
+    Country foreign key referencing Teams
+DOB NOT NULL
+role NOT NULL
+
+Referees (rname, year_of_experience, country)
+	year_of_experience NOT NULL
+	country NOT NULL
+
+Stadiums(sname, location, capacity)
+	location NOT NULL
+capacity NOT NULL
+
+Games(id, date, time, length, round, stadium, playing_country, opposing_country)
+	date NOT NULL
+	time NOT NULL
+length NOT NULL
+round NOT NULL
+stadium NOT NULL foreign key referencing stadiums(name)
+	playing_country NOT NULL foreign key referencing Teams(country)
+	opposing_country NOT NULL foreign key referencing Teams(country)
+
+Goals(id, occurrence, minute, penalty, shirt_num, country)
+	Id foreign key referencing Games
+	minute NOT NULL
+penalty NOT NULL
+	shirt_num, country NOT NULL foreign key referencing Players(shirt_num, country)
+
+Refs_game (rname,id, role)
+	rname foreign key referencing Referees
+	id foreign key referencing Games
+role NOT NULL
+
+Performs(pname, id, etime, ltime, position, Rcard, Ycard)
+	pname foreign key referencing Players
+	id foreign key referencing Games
+	etime NOT NULL
+	ltime NOT NULL
+position NOT NULL
+Rcard NOT NULL
+	Ycard NOT NULL
 
