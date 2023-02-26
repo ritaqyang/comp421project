bbbed3b332a0b5471a30f4f9d7b65f01c63142fd
ritaqyang <ritaqyang@gmail.com>
2023-02-26 08:37:22 -0500

    q4

--- queries/src/q4.sql
+++ queries/src/q4.sql
@@ -1,8 +1,7 @@
 SELECT *
 FROM Purchases P;
 
-SELECT B.city, B.province, B.country,
-FROM Billing_addresses B;
+
 
 
 
@@ -13,7 +12,24 @@ WHERE T.tid = P.tid AND T.gname LIKE '%France%' AND T.tid in (SELECT S.tid
                                             WHERE S.sold_price > 150);
 
 
-SELECT *
+
+SELECT B.city, B.province, B.country, B.name, B.email
+FROM Billing_addresses B
+WHERE B.email IN (SELECT P.email
+                  FROM Purchases P
+                  WHERE P.transaction_id IN (SELECT transaction_id
+                                             FROM (SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
+                                                   FROM Purchases P
+                                                   WHERE P.transaction_id IN (SELECT DISTINCT P.transaction_id
+                                                                              FROM Tickets T,Purchases P
+                                                                              WHERE T.tid = P.tid AND T.gname LIKE '%France%' AND T.tid in (SELECT S.tid
+                                                                                                                                            FROM Sold_tickets S
+                                                                                                                                            WHERE S.sold_price > 150))
+                                                   GROUP BY P.transaction_id)
+                                             WHERE num_tickets > 3));
+
+
+SELECT transaction_id
 FROM (SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
       FROM Purchases P
       WHERE P.transaction_id IN (SELECT DISTINCT P.transaction_id
@@ -22,7 +38,7 @@ FROM (SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
                                                                                                FROM Sold_tickets S
                                                                                                WHERE S.sold_price > 150))
       GROUP BY P.transaction_id)
-WHERE num_tickets > 2;
+WHERE num_tickets > 3;
 
 SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
 FROM Purchases P
