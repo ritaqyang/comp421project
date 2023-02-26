f7aca7700bc084aa750a435195527f667e7d76ff
ritaqyang <ritaqyang@gmail.com>
2023-02-26 08:32:14 -0500

    q4

--- queries/src/q4.sql
+++ queries/src/q4.sql
@@ -1,2 +1,35 @@
 SELECT *
 FROM Purchases P;
+
+SELECT B.city, B.province, B.country,
+FROM Billing_addresses B;
+
+
+
+SELECT DISTINCT P.transaction_id
+FROM Tickets T,Purchases P
+WHERE T.tid = P.tid AND T.gname LIKE '%France%' AND T.tid in (SELECT S.tid
+                                            FROM Sold_tickets S
+                                            WHERE S.sold_price > 150);
+
+
+SELECT *
+FROM (SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
+      FROM Purchases P
+      WHERE P.transaction_id IN (SELECT DISTINCT P.transaction_id
+                                 FROM Tickets T,Purchases P
+                                 WHERE T.tid = P.tid AND T.gname LIKE '%France%' AND T.tid in (SELECT S.tid
+                                                                                               FROM Sold_tickets S
+                                                                                               WHERE S.sold_price > 150))
+      GROUP BY P.transaction_id)
+WHERE num_tickets > 2;
+
+SELECT P.transaction_id, COUNT(DISTINCT P.tid) AS num_tickets
+FROM Purchases P
+WHERE P.transaction_id IN (SELECT DISTINCT P.transaction_id
+                           FROM Tickets T,Purchases P
+                           WHERE T.tid = P.tid AND T.gname LIKE '%France%' AND T.tid in (SELECT S.tid
+                                                                                         FROM Sold_tickets S
+                                                                                         WHERE S.sold_price > 150))
+GROUP BY P.transaction_id;
+
