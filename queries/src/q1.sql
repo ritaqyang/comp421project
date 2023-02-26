7481741704a72f4bfc21443a15e48ac470325c39
ritaqyang <ritaqyang@gmail.com>
2023-02-26 06:10:18 -0500

    q1

--- queries/src/q1.sql
+++ queries/src/q1.sql
@@ -12,6 +12,23 @@ WHERE S.sname = G2.stadium AND G2.id IN ( SELECT G.id
                                                                FROM Players P
                                                                WHERE pname = 'Christine Sinclair'));
 
+SELECT S.sname, S.location, G2.date
+FROM Stadiums S, Games G2
+WHERE S.sname = G2.stadium AND G2.id IN ( SELECT id
+                                          FROM Goals
+                                          WHERE country IN (SELECT P.country
+                                                              FROM Players P
+                                                              WHERE P.pname = 'Christine Sinclair')
+                                          INTERSECT
+                                          SELECT G.id
+                                          FROM Goals G
+                                          WHERE G.shirt_num IN(SELECT P.shirt_num
+                                                               FROM Players P
+                                                               WHERE pname = 'Christine Sinclair'));
+
+
+SELECT sname, location, date FROM Stadiums JOIN Games ON Stadiums.sname = Games.stadium WHERE id in (SELECT id FROM Goals WHERE country = (SELECT country FROM Players WHERE pname = 'Christine Sinclair') AND shirt_num = (SELECT shirt_num FROM Players WHERE pname = 'Christine Sinclair'));
+
 
 SELECT *
 FROM Games G
