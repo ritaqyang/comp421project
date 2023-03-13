3782264d15b9d1f3f2b931cfa1cf653c1e3d5a48
ritaqyang <ritaqyang@gmail.com>
2023-03-13 16:50:37 -0400

    updated loaddata.sql

--- project2_combined/modified_for_p3/loaddata.sql
+++ project2_combined/modified_for_p3/loaddata.sql
@@ -451,66 +451,6 @@ INSERT INTO Customers(email,password) VALUES
 
 
 
-INSERT INTO Stadium_addresses(aid, address, city, province, country)
-VALUES
-
-    (4300111278,'30 Haas Terrace','Aulnay-sous-Bois','Île-de-France','South Korea'),
-    (3886517047,'1 Continental Trail','Philadelphia','Pennsylvania','Portugal'),
-    (4332054679,'22945 Rutledge Place','Marseille','Provence-Alpes-Côte dAzur','Poland'),
-    (8688273111,'362 Del Sol Park','La Rochelle','Poitou-Charentes','Democratic Republic of the Congo'),
-    (3828333176,'032 Susan Trail','Saint Louis','Missouri','France'),
-    (5412898149,'9994 Upham Drive','Progreso','Chihuahua','Brazil'),
-    (9620273540,'67 Boyd Road','Laon','Picardie','Bosnia and Herzegovina'),
-    (7608900458,'1090 American Road','Helsingborg','Skåne','Indonesia'),
-    (3520850591,'74 Erie Park','Kuantan','Pahang','Sweden'),
-    (8626512082,'947 Springview Center','Meirinhas','Leiria','Sudan'),
-    (7705644088,'56 Badeau Circle','Vagos','Aveiro','Belarus'),
-    (0404540384,'4 Montana Place','Oslo','Oslo','Czech Republic'),
-    (7486090945,'023 Arizona Crossing','Fovissste','Puebla','China'),
-    (0113422091,'83 Di Loreto Parkway','Vales','Viseu','Thailand'),
-    (8590006611,'01619 Alpine Pass','Gävle','Gävleborg','Sweden'),
-    (3083782373,'40 Melvin Park','Albertville','Rhône-Alpes','Ukraine'),
-    (8547928316,'69 Granby Lane','Borlänge','Dalarna','France'),
-    (2082244385,'98 Northfield Avenue','Caen','Basse-Normandie','Malaysia'),
-    (1179969065,'38581 Thompson Terrace','Lapas','Santarém','Malaysia'),
-    (5118418623,'4238 Jenifer Center','Soutelo','Braga','Philippines'),
-    (1522078282,'1 Sycamore Trail','Messejana','Beja','United States'),
-    (5783659177,'3080 Melrose Way','Guimarei','Porto','Pakistan'),
-    (9982780999,'2 Stone Corner Avenue','Ivry-sur-Seine','Île-de-France','Poland'),
-    (4415356605,'056 Hanover Avenue','Nantes','Pays de la Loire','China');
-
-
-
-INSERT INTO Games_P2(gname, date, time, aid)
-VALUES
-
-    ('France vs.Portugal 2023-07-20','2023-07-20','4:04 AM',4300111278),
-    ('United States vs.Sweden 2023-07-28','2023-07-28','12:28 AM',3886517047),
-    ('France vs.Portugal 2023-06-08','2023-06-08','11:58 PM',4332054679),
-    ('France vs.United States 2023-06-28','2023-06-28','11:13 PM',8688273111),
-    ('United States vs.Guadeloupe 2023-06-29','2023-06-29','2:27 AM',3828333176),
-    ('Mexico vs.Netherlands 2023-06-19','2023-06-19','8:37 AM',5412898149),
-    ('France vs.Sweden 2023-08-04','2023-08-04','6:51 PM',9620273540),
-    ('Sweden vs.Canada 2023-07-14','2023-07-14','12:11 AM',7608900458),
-    ('Malaysia vs.France 2023-08-05','2023-08-05','8:29 PM',3520850591),
-    ('Portugal vs.Canada 2023-07-26','2023-07-26','7:59 AM',8626512082),
-    ('Portugal vs.France 2023-07-26','2023-07-26','6:53 PM',7705644088),
-    ('Norway vs.Sweden 2023-08-01','2023-08-01','12:11 PM',0404540384),
-    ('Mexico vs.United States 2023-08-02','2023-08-02','12:39 PM',7486090945),
-    ('Portugal vs.Belgium 2023-06-27','2023-06-27','4:19 AM',0113422091),
-    ('Sweden vs.Portugal 2023-06-19','2023-06-19','10:54 PM',8590006611),
-    ('France vs.Portugal 2023-06-14','2023-06-14','12:26 PM',3083782373),
-    ('Sweden vs.Portugal 2023-07-26','2023-07-26','6:05 PM',8547928316),
-    ('France vs.Portugal 2023-07-09','2023-07-09','2:52 AM',2082244385),
-    ('Portugal vs.Portugal 2023-07-28','2023-07-28','9:45 AM',1179969065),
-    ('Portugal vs.France 2023-06-15','2023-06-15','12:22 PM',5118418623),
-    ('Portugal vs.France 2023-08-02','2023-08-02','3:53 PM',1522078282),
-    ('Portugal vs.United States 2023-07-06','2023-07-06','9:23 PM',5783659177),
-    ('France vs.France 2023-06-06','2023-06-06','2:26 AM',9982780999),
-    ('France vs.Portugal 2023-07-23','2023-07-23','12:40 PM',4415356605);
-
-
-
 INSERT INTO Billing_addresses(aid, address, address2, city, province, country, postal_code, phone, name,email)
 VALUES
 
